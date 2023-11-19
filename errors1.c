e23c44b71c04a97aa3bcb527bdf6339801ac6de7
amineNouabi <aminenouabi9@gmail.com>
2023-11-19 02:09:12 +0100

    Fixed errors

--- errors1.c
+++ errors1.c
@@ -1,7 +1,8 @@
 #include "monty.h"
 
 /**
- * handle_error - Prints appropriate error messages determined by their error code.
+ * handle_error - Prints appropriate error
+ *					messages determined by their error code.
  * @err_code: The error codes are the following:
  * (1) => No input file or more than one file provided to the program.
  * (2) => Unable to open or read the provided file.
@@ -15,47 +16,62 @@
  */
 void handle_error(int err_code, ...)
 {
-    va_list args;
-    char *instruction;
-    int line_num;
+	va_list args;
 
-    va_start(args, err_code);
-    switch (err_code)
-    {
-    case 1:
-        fprintf(stderr, "USAGE: monty file\n");
-        break;
-    case 2:
-        fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
-        break;
-    case 3:
-        line_num = va_arg(args, int);
-        instruction = va_arg(args, char *);
-        fprintf(stderr, "L%d: unknown instruction %s\n", line_num, instruction);
-        break;
-    case 4:
-        fprintf(stderr, "Error: Memory allocation (malloc) failed\n");
-        break;
-    case 5:
-        fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
-        break;
-    case 6:
-        fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(args, int));
-        break;
-    case 7:
-        fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(args, int));
-        break;
-    case 8:
-        line_num = va_arg(args, unsigned int);
-        instruction = va_arg(args, char *);
-        fprintf(stderr, "L%d: can't %s, stack too short\n", line_num, instruction);
-        break;
-    case 9:
-        fprintf(stderr, "L%d: division by zero\n", va_arg(args, unsigned int));
-        break;
-    default:
-        break;
-    }
-    free_stack_nod();
-    exit(EXIT_FAILURE);
+
+	va_start(args, err_code);
+	error_switch(err_code, args);
+
+	free_stack_nod();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * error_switch - show appropriate error message based on error_code
+ * @error_code: int representing which error to trigger
+ * @args: line number and parametre if given
+ *
+ * Return: void
+ */
+void error_switch(int error_code, va_list args)
+{
+	char *instruction;
+	int line_num;
+
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n", va_arg(args, char *));
+			break;
+		case 3:
+			line_num = va_arg(args, int);
+			instruction = va_arg(args, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, instruction);
+			break;
+		case 4:
+			fprintf(stderr, "Error: Memory allocation (malloc) failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
+			break;
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(args, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(args, int));
+			break;
+		case 8:
+			line_num = va_arg(args, unsigned int);
+			instruction = va_arg(args, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", line_num, instruction);
+			break;
+		case 9:
+			fprintf(stderr, "L%d: division by zero\n", va_arg(args, unsigned int));
+			break;
+		default:
+			break;
+	}
 }
